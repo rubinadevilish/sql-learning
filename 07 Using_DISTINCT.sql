4254cc73e727782253c50f47b7c32b946add3ab6
Rubina Khan <rubinadevilish@gmail.com>
2026-09-12 19:46:22 +0530

    Add 07 distinct query clause

---
+++ 07 Using_DISTINCT.sql
@@ -0,0 +1,6 @@
+-- Project: SQL Learning Journey
+-- {DISTINCT remove duplicates. Don't use DISTINCT unless it's necessary; as it can slow down your query}
+
+/* Return unique list of countries */
+SELECT DISTINCT country
+FROM customers
