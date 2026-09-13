58efe3ed6e1da95a849958dfd6d3364b06b91064
Rubina Khan <rubinadevilish@gmail.com>
2026-09-13 17:04:36 +0530

    Add delete or truncate clause query

---
+++ 13 Using_DELETE_TRUNCATE.sql
@@ -0,0 +1,12 @@
+-- Project: SQL Learning Journey
+/* Delete all customers with an ID greater than 5 */
+DELETE FROM customers
+WHERE id> 5
+
+
+/* Delete all data from table persons */
+DELETE FROM persons
+
+TRUNCATE TABLE persons
+
+-- TRUNCATE clears the whole table at once without checking or logging
