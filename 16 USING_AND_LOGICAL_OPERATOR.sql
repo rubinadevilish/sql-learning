802f2fc19ac7e738b8d54cbf867f724c56129a63
Rubina Khan <rubinadevilish@gmail.com>
2026-09-17 16:58:25 +0530

    ADD AND clause query

---
+++ 16 USING_AND_LOGICAL_OPERATOR.sql
@@ -0,0 +1,9 @@
+-- Project: SQL Learning Journey
+/* Retrieve all customers who are from USA and have a score greater than 500 */
+
+SELECT *
+FROM customers
+WHERE country = 'USA' AND score > 500
+
+
+/* AND All conditions must be true */
