09ec132275030a13c253908c4d6e75e789db8072
Rubina Khan <rubinadevilish@gmail.com>
2026-09-17 18:01:03 +0530

    ADD types of where operators

---
+++ WHERE_OPERATORS.sql
@@ -0,0 +1,7 @@
+-- Project: SQL Learning Journey
+/* WHERE operators
+Comparison Operators  =   <> =!   >   >=   <   <=
+Logical Operators AND   OR   NOT
+Range Operator BETWEEN
+Membership Operator IN   NOT IN
+Search Operator LIKE 
