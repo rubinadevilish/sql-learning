7ccd7169cec936314865541b8c106c15ce59b34f
Rubina Khan <rubinadevilish@gmail.com>
2026-09-17 18:40:50 +0530

    Using NO JOINS query

---
+++ 22_USING_NO_JOINS.sql
@@ -0,0 +1,11 @@
+-- Project: SQL Learning Journey
+
+/* RETRIEVE all data from customers and orders in two different results */
+
+SELECT *
+FROM customers
+
+SELECT *
+FROM orders
+
+/* NO JOIN returns data from tables without combining them
