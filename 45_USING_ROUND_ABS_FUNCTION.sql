63ba66bcafc05ee3d6466e1848c42489db210a2f
Rubina Khan <rubinadevilish@gmail.com>
2026-09-25 11:46:48 +0530

    add NUMERIC function query

---
+++ 45_USING_ROUND_ABS_FUNCTION.sql
@@ -0,0 +1,12 @@
+-- Project: SQL Learning Journey
+SELECT 
+    3.516 As Number,
+    ROUND (3.516, 2) AS round_2,
+    ROUND (3.516, 1) AS round_1,
+    ROUND (3.516, 0) AS round_0
+
+SELECT
+-10,
+ABS (-10),
+ABS (10)
+
